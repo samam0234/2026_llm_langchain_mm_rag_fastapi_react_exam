77068fd6dc05086320c920ea1f84a761a4ae67ac
while <limjinheng0120@gmail.com>
2026-06-01 09:31:38 +0900

    feat : LLM의 visionModel을 사용하여 이미지를 분석시키고, 응답 처리 + image rag에 저장하고 검색하는 방법

--- part04_multimodal/ch04_01_visionModel_basic.py
+++ part04_multimodal/ch04_01_visionModel_basic.py
@@ -39,12 +39,12 @@ def base64_to_image(b64:str, save_path: str) -> None :
     out_path.write_bytes(image_bytes)   # bytes -> file write
 
 
+if __name__=="__main__" :
+    b64, mt = image_to_base64("./vision_sample/cat.jpeg")
+    print(f"media_type : {mt}")
+    print(f"Base64 길이: {len(b64)} 문자")
+    print(f"앞 30자    : {b64[:30]}...")
+    print(f"data URI   : data:{mt};base64,{b64[:20]}...")
 
-b64, mt = image_to_base64("./vision_sample/cat.jpeg")
-print(f"media_type : {mt}")
-print(f"Base64 길이: {len(b64)} 문자")
-print(f"앞 30자    : {b64[:30]}...")
-print(f"data URI   : data:{mt};base64,{b64[:20]}...")
-
-# b64에 들어있는 base64를 디코딩하여 "./vision_sample/decode_exam.jpeg"로 저장하는 함수를 만드세요
-base64_to_image(b64, "./vision_sample/decode_exam.jpeg")
+    # b64에 들어있는 base64를 디코딩하여 "./vision_sample/decode_exam.jpeg"로 저장하는 함수를 만드세요
+    base64_to_image(b64, "./vision_sample/decode_exam.jpeg")
